0f702336305656c82f73c741aaa20a5ce37b5f20
hubert <xnatural@msn.cn>
2020-12-12 21:27:12 +0800

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+### 介绍
+remoter: 轻量级远程调用库. 基于 [enet](https://gitee.com/xnat/enet) 和 [aio](https://gitee.com/xnat/aio)
+
+
+### 参与贡献
+xnatural@msn.cn
---
+++ src/test/main/java/RemoterTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+public class RemoterTest {
+
+    @Test
+    void taskTest() throws Exception {
+
+    }
+
+
+
+}
